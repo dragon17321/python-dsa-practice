67771687b6ed7eaa384badd642361d0a66b3f5da
dragon17321 <ipnitheshraj21@gmail.com>
2025-07-30 16:05:42 +0000

    pattern printing problem 2

---
+++ pattern2.py
@@ -0,0 +1,11 @@
+#patternn printing2
+n=int(input()
+    )
+for row in range(n):
+      for num in range(n):
+          if num<=row:
+              print(num+1, end="")
+          else:
+              print(" ",end=" ")                       
+      print("")
+       
